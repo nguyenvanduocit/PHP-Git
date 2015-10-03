0283e6897b6e6037fcfd8f7ba1057034d38e9524
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-10-04 00:33:59 +0700

    Init

---
+++ src/PHPGit/PHPGit.php
@@ -0,0 +1,110 @@
+<?php
+namespace PHPGit;
+use Symfony\Component\Process\Process;
+
+class PHPGit {
+	/** @var string path to git bin*/
+	protected static $binPath = null;
+	/** @var string path to repo */
+	protected $repoPath = null;
+	/** @var ProcessBuilder Command builder*/
+	protected $processBuilder = null;
+	/** @var Process the last process */
+	protected $lastProcess = null;
+	public function __construct( $repoPath, $binPath = null ){
+		$this->repoPath = $repoPath;
+		if(!$binPath){
+			$binPath = $this->detectBinPath();
+		}
+		static::setBinPath($binPath);
+		$this->processBuilder = new ProcessBuilder(static::$binPath, $this->repoPath);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function detectBinPath(){
+		switch(PHP_OS){
+			case 'DAR':
+			case 'LINUX':
+				return '/usr/local/git';
+			case 'WIN':
+				return 'git';
+			default:
+				throw new \InvalidArgumentException('Can not detect bin path');
+		}
+	}
+
+	/**
+	 * Excute a command
+	 * @return Process
+	 */
+	public function exec(){
+		$args = func_get_args();
+		$this->lastProcess = $this->getProcessBuilder()->getProcess($args);
+		$this->lastProcess->run();
+		return $this->lastProcess;
+	}
+	/**
+	 * get $binPath
+	 *
+	 * @return string
+	 */
+	public static function getBinPath() {
+		return self::$binPath;
+	}
+
+	/**
+	 * set $binPath
+	 *
+	 * @param string $binPath
+	 */
+	public static function setBinPath( $binPath ) {
+		self::$binPath = $binPath;
+	}
+
+	/**
+	 * get repoPath
+	 *
+	 * @return string
+	 */
+	public function getRepoPath() {
+		return $this->repoPath;
+	}
+
+	/**
+	 * set repoPath
+	 *
+	 * @param string $repoPath
+	 */
+	public function setRepoPath( $repoPath ) {
+		$this->repoPath = $repoPath;
+	}
+
+	/**
+	 * get processBuilder
+	 *
+	 * @return ProcessBuilder
+	 */
+	public function getProcessBuilder() {
+		return $this->processBuilder;
+	}
+
+	/**
+	 * Set processBuilder
+	 *
+	 * @param ProcessBuilder $processBuilder
+	 */
+	public function setProcessBuilder( $processBuilder ) {
+		$this->processBuilder = $processBuilder;
+	}
+
+	/**
+	 * @return Process
+	 */
+	public function getLastProcess() {
+		return $this->lastProcess;
+	}
+
+
+}
